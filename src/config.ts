c6c19fda77ca7e8f7f2955f7169d556b78cc0f0a
Sandro <sandro.rmv@gmail.com>
2024-11-27 19:29:23 -0300

    feat: Adicionando configuração para acesso em rede

---
+++ src/config.ts
@@ -0,0 +1,14 @@
+const API_PORT = 8081;
+const VITE_PORT = 5173;
+const API_HOST = '172.21.48.1';
+
+// URLs base para diferentes serviços
+const API_URL = `http://${API_HOST}:${API_PORT}`;
+const FRONTEND_URL = `http://${API_HOST}:${VITE_PORT}`;
+
+export const config = {
+  apiUrl: API_URL,
+  uploadsUrl: API_URL,
+  frontendUrl: FRONTEND_URL,
+  baserowToken: '0lsB6U6zcpKt8W4f9pydlsvJnibOASeI'
+};
